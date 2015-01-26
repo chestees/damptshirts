80489ce69e6ecb2b0df6c9bf809bfcb3e7bdfc19
Jason Diehl <jdiehl@exacttarget.com>
2015-01-26 10:16:42 -0500

    changed port settings for heroku

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 var express   = require('express'),
-  http        = require('http'),
+  //http        = require('http'),
   restful     = require('node-restful'),
   mongoose    = restful.mongoose,
   // bodyParser  = require('body-parser'),
   serveStatic = require('serve-static');
 
-var db = mongoose.connection;
+var db    = mongoose.connection;
 var app   = express();
 var dbUrl = process.env.DATABASE_URL;
 
 // app.use( bodyParser() );
 app.use( serveStatic('public') );
+
+app.set('port', (process.env.PORT || 3001));
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
 
 mongoose.connect( dbUrl );
 
@@ -35,8 +41,4 @@ Shirts.register(app, '/api/shirts');
 
 app.get('/*', function  (req, res) {
 	res.json(404, {status: 'Nobody here by that name.'});
-} );
-
-http.createServer(app).listen(3001, function () {
-  console.log("Server ready at http://localhost:3001");
 } );
